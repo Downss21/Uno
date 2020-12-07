87686a4b0687e75dc87b4ffbc1321d5e774a8bea
Sam Downs <downss21@stxtigers.com>
2020-12-07 17:01:56 -0500

    where does this go?

---
+++ src/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+	private String number;
+	private String color;
+	private boolean action;
+	
+	public Card(String number, String color) {
+		this.number = number;
+		this.color = color;
+	}
+}
---
+++ src/Helper.java
@@ -0,0 +1,5 @@
+public class Helper {
+	public static final String[] CARDS = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Skip","Reverse","+2","Wild","+4"};
+	public static final String[] ACTION_CARDS = {"Skip","Reverse","+2","Wild","+4"};
+	public static final String[] NUMBER_CARDS = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+}

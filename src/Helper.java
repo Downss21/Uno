8517ebbf0612a8ed61d193050c1ea5d447e777fa
Sam Downs <downss21@stxtigers.com>
2020-12-07 21:25:11 -0500

    made some sets

--- src/Helper.java
+++ src/Helper.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Helper {
-	public static final String[] CARDS = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Skip","Reverse","+2","Wild","+4"};
-	public static final String[] ACTION_CARDS = {"Skip","Reverse","+2","Wild","+4"};
-	public static final String[] NUMBER_CARDS = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+	public static final Set<String> CARDS = new HashSet<String>(Arrays.asList(
+			"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Skip","Reverse","+2","Wild","+4"
+			));
+	public static final Set<String> ACTION_CARDS = new HashSet<String>(Arrays.asList(
+			"Skip","Reverse","+2","Wild","+4"
+			));
+	public static final Set<String> NUMBER_CARDS =  new HashSet<String>(Arrays.asList(
+			"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"
+			));
+	public static final String[] UNIQUE_CARDS = {"Zero","Wild","+4"};
 }

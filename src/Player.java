3420fc9b84a3e8d8ad756a64f3599aa0041322dd
Sam Downs <downss21@stxtigers.com>
2020-12-11 14:12:58 -0500

    finished main player class

--- src/Player.java
+++ src/Player.java
@@ -2,10 +2,39 @@
 public class Player {
 	String name;
 	int points;
+	Hand hand;
 	
-	public Player()
+	public Player(String name, Deck deck)
 	{
-		
+		this.name = name;
+		this.points = 0;
+		this.hand = new Hand(deck);
 	}
 
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the points
+	 */
+	public int getPoints() {
+		return points;
+	}
+
+	/**
+	 * @return the hand
+	 */
+	public Hand getHand() {
+		return hand;
+	}
+	public void addPoints(int reward) {
+		this.points +=reward;
+	}
+	public void resetHand(Deck deck) {
+		this.hand = new Hand(deck);
+	}
 }
